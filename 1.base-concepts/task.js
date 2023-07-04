c8b249124d4255fc3f2dad4f3334b0d67695559a
Emma ParisAlyona <churakovaalyona@gmail.com>
2023-07-04 13:21:35 +0200

    changed task.js

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,32 @@
 "use strict"
+
 function solveEquation(a, b, c) {
-  let arr = [];
-  
-  return arr;
+	let result = []
+	let discriminant = Math.pow(b, 2) - 4 * a * c;
+	if (discriminant === 0) {
+		let root = (-b) / (2 * a);
+		result.push(root)
+		return result
+	} else if (discriminant > 0) {
+		let root1 = ((-b) + Math.sqrt(discriminant)) / (2 * a)
+		let root2 = ((-b) - Math.sqrt(discriminant)) / (2 * a)
+		result.push(root1, root2)
+		return result
+	}
+}
+return result
+
+
+
+function calculateTotalMortgage(percent, contribution, amount, monthsQuantity) {
+	const monthlyPercent = percent / 100 / 12;
+	const loanAmount = amount - contribution;
+	const monthlyPayment = loanAmount * (monthlyPercent + (monthlyPercent / (Math.pow(1 + monthlyPercent, monthsQuantity) - 1)));
+
+	const totalPayment = monthlyPayment * monthsQuantity;
+	const roundedTotalPayment = Math.round(totalPayment * 100) / 100;
+	return roundedTotalPayment;
 }
 
-function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
-}
+const totalPayment = calculateTotalMortgage(10, 20000, 150000, 12);
+console.log(totalPayment);
